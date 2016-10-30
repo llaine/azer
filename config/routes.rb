b5a6de506bd84f83f5f09496b59e8fc8034b50a9
Louis Lainé <darksioul6@gmail.com>
2016-10-30 12:13:58 +0100

    views

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
       get :public
     end
   end
-
+  resources :offers do
+    member do
+      post 'join', to: 'crew_merger#join_crew'
+      post 'leave', to: 'crew_merger#leave_crew'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
